41fbf50a921590d0bbc1f8059df2304fe23da8bc
devEstivenValencia <dev@estivenvalencia.co>
2025-01-18 13:19:37 -0500

    fix(uri): validar que los parámetros de búsqueda no estén vacíos antes de generar la URI

--- src/common/utils/generate-uri.ts
+++ src/common/utils/generate-uri.ts
@@ -1,7 +1,7 @@
 import { BaseSearchParams, genURLSearchParams } from './generate-search-params'
 
 export function generateUri(url: string, searchParams?: BaseSearchParams) {
-	if (searchParams) {
+	if (searchParams && searchParams?.length > 0) {
 		return url + '?' + genURLSearchParams(searchParams)
 	}
 
